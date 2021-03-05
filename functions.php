c863e9af7e222dee109c1e3c6e949a74c87aa538
Ostap Letnyanchyk <ostap.letnyanchyk@gmail.com>
2021-03-05 14:32:40 +0200

    add_shortcode my_oferta

--- functions.php
+++ functions.php
@@ -101,3 +101,34 @@ function show_footer_sub_menu() {
 
       ';
 }
+
+add_shortcode( 'my_oferta', 'show_oferta' );
+
+function show_oferta(){
+
+  echo '<div id="oferta" class="oferta-wrap">
+          <ul class="oferta-list">';
+
+    $args = array(
+      'post_type' => 'page',
+      'post_parent' => 179,
+    );
+    
+    $query = new WP_Query( $args );
+    
+    if ( $query->have_posts() ) {
+      while ( $query->have_posts() ) {
+        $query->the_post();
+        echo '<li class="oferta-item"><a href="' . get_permalink() . '" class="oferta-link"><i class="icon ' . get_the_ID() . '"></i><span class="oferta-title">' . get_the_title() . '</span></a></li>
+            ';
+      }
+    } else {
+      // Постов не найдено
+    }
+
+    wp_reset_postdata();
+
+    echo '</ul>
+        </div>';
+
+}
